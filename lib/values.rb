fd500029f886af686a47cdb0988472e2df8dc847
Elizabeth Cobb <cobbeec@gmail.com>
2020-08-29 00:11:41 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -21,7 +21,7 @@ def get_the_keys (groceries)
  groceries.keys
  end 
  
- def get_the_min 
+ def get_the_min (groceries)
   groceries = {
   dairy: ["milk", "yogurt", "cheese"],
   vegetable: ["carrots", "broccoli", "cucumbers"],
